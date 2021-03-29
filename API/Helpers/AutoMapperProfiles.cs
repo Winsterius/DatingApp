b253b9e87e1e7a6cfd130c4647714670ebec5d93
Winsterius <jura.sch90@gmail.com>
2021-03-29 19:25:35 +0300

    End of Section 18

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -26,7 +26,6 @@ namespace API.Helpers
                 .ForMember(dest => dest.RecipientPhotoUrl, opt => opt.MapFrom(
                             src => src.Recipient.Photos.FirstOrDefault(p => p.IsMain).Url
                 ));
-            CreateMap<DateTime, DateTime>().ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
         }        
     }
 }
